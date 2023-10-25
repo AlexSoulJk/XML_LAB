c4a62d0b08a154c8910b44cc88e0bc1e74d1bca7
AlexSoulJk <alex.solomatow2016@yandex.ru>
2023-10-25 17:20:40 +0300

    Fix1

--- XML_Resources/Node.cpp
+++ XML_Resources/Node.cpp
@@ -1,1 +1,20 @@
 #include "Node.h"
+std::string Node::stringify(const int depth = 0) {
+    const std::string indent = std::string(" ", depth * 2);
+    std::string result = "";
+
+    result += indent + "<" + tag + ">" + "value=" + value;
+    if (children.size()) {
+        result += "\n";
+    }
+    std::cout << tag << " " << "\"" << "value=" + value << "\"" << " " << depth << std::endl;
+    for (const auto& child : children) {
+        result += child->stringify(depth + 1);
+    }
+    if (children.size()) {
+        result += indent;
+    }
+    result += "</" + tag + ">" + "\n";
+
+    return result;
+};
--- XML_Resources/Node.h
+++ XML_Resources/Node.h
@@ -18,23 +18,5 @@ public:
     void append(std::unique_ptr<Node> child) {
         children.emplace_back(std::move(child));
     }
-    std::string stringify(const int depth = 0) {
-        const std::string indent = std::string(" ", depth * 2);
-        std::string result = "";
-
-        result += indent + "<" + tag + ">" + "value=" + value;
-        if (children.size()) {
-            result += "\n";
-        }
-        std::cout << tag << " " << "\"" << "value=" + value << "\"" << " " << depth << std::endl;
-        for (const auto& child : children) {
-            result += child->stringify(depth + 1);
-        }
-        if (children.size()) {
-            result += indent;
-        }
-        result += "</" + tag + ">" + "\n";
-
-        return result;
-    };
+    std::string stringify(const int depth = 0);
 };
